7ad74d82449e6c58218f0be5a71137f13a960a74
Алексей Андрюшин <aleksroebuck@gmail.com>
2022-01-06 22:30:02 +0200

    Made connection through foreign keys

--- products/models.py
+++ products/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.urls import reverse
 
 
 class Category(models.Model):
@@ -11,18 +10,18 @@ class Category(models.Model):
 
 
 class Subgroup(models.Model):
-    category = models.CharField(max_length=10)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
 
 
 class Subsubgroup(models.Model):
-    sub_group = models.CharField(max_length=10)
+    sub_group = models.ForeignKey(Subgroup, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    category = models.CharField(max_length=10)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
 class Screw(models.Model):
-    category = models.ForeignKey(Category, related_name='screws', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     name = models.CharField(max_length=200, verbose_name="Screw name")
     group_standard = models.IntegerField(verbose_name='Standard (group)', null=True)
@@ -37,8 +36,8 @@ class Screw(models.Model):
     bit_size = models.CharField(max_length=50, verbose_name='Bit size')
     base = models.CharField(max_length=50, verbose_name='For what base')
     spline = models.CharField(max_length=50, verbose_name='Spline')
-    sub_group = models.CharField(max_length=10)
-    subsub_group = models.CharField(max_length=10)
+    sub_group = models.ForeignKey(Subgroup, on_delete=models.CASCADE, null=True)
+    subsub_group = models.ForeignKey(Subsubgroup, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
